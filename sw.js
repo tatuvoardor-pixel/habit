7edc415cc5bc0875d222ee85bb7fb8b28cbac3dc
tatuvoardor-pixel <tatuvoardor@gmail.com>
2026-06-06 17:36:29 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'charge-v1';
+const CACHE = 'charge-v4';
 const FILES = ['.', './index.html', './manifest.json', './icon.svg'];
 
 self.addEventListener('install', e => {
@@ -18,15 +18,10 @@ self.addEventListener('activate', e => {
 self.addEventListener('fetch', e => {
   e.respondWith(
     caches.match(e.request).then(r => r || fetch(e.request).then(res => {
-      if (!res || res.status !== 200 || res.type !== 'basic') return res;
+      if(!res || res.status !== 200 || res.type !== 'basic') return res;
       const clone = res.clone();
       caches.open(CACHE).then(c => c.put(e.request, clone));
       return res;
     }))
   );
-});
-
-self.addEventListener('notificationclick', e => {
-  e.notification.close();
-  e.waitUntil(clients.openWindow('./'));
 });
